e67ba83ae7fe175575f6c151cd06b09ac83a0ac0
vipetrone <vipetrone@unisa.it>
2022-03-26 15:17:23 +0100

    [DOC] Adds documentation to functions

--- functions/free_evolution.m
+++ functions/free_evolution.m
@@ -1,6 +1,8 @@
 function [Yel] = free_evolution(A,C,x)
-%FREE_EVOLUTION Summary of this function goes here
-%   Detailed explanation goes here
+%FREE_EVOLUTION Computes the Laplace transform of the free evolution of the
+%state-space system defined by the dynamic matrices A and C, starting from 
+%the state X
+%   The function implements the equazion Yel(s) = C (sI-A)^-1 x
 syms s
 I = eye(size(A));
 Yel(s) = C / (s*I - A) * x;
--- functions/transfer_function.m
+++ functions/transfer_function.m
@@ -1,6 +1,9 @@
 function [G] = transfer_function(A,B,C,D)
-%TRANSFER_FUNCTION Summary of this function goes here
-%   Detailed explanation goes here
+%TRANSFER_FUNCTION Computes the Laplace Transfer Function of the
+%state-space system described by the dynamic matrices A,B,C,D
+%   The function implements the equation G(s) = C (sI-A)^-a B + D, such
+%   that the response is Y(s) = G(s) U(s), where U(s) is the Laplace
+%   transform of an input
 syms s
 I = eye(size(A));
 G(s) = C / (s*I - A) * B + D;
