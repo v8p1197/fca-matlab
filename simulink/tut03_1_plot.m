408a6775772c6854bce3024c36d15d0fa89b3e67
vipetrone <vipetrone@unisa.it>
2022-04-05 13:36:21 +0200

    [ADD] Exports time switch model

---
+++ simulink/tut03_1_plot.m
@@ -0,0 +1,25 @@
+close all;
+
+FS = 15;
+LW = 2;
+
+figure;
+
+subplot(3,1,1)
+plot(out.y1)
+grid on
+legend('y1', 'Location','best', 'FontSize', FS);
+
+subplot(3,1,2)
+plot(out.y2)
+grid on
+legend('y2', 'Location','best', 'FontSize', FS);
+
+subplot(3,1,3)
+plot(out.y)
+grid on
+legend('y', 'Location','best', 'FontSize', FS);
+
+set(findall(gcf,'-property','FontSize'),'FontSize',FS)
+set(findall(gcf,'Type','Line'),'LineWidth',LW)
+set(findobj(lgd, 'Type','Line'),'LineWidth',LW)
