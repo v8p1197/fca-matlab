1814b0678abf8097977d9d11d3392917846e2a31
vipetrone <vipetrone@unisa.it>
2022-04-08 14:28:22 +0200

    [REF] Modifies simulin model

---
+++ simulink/ex_04_04_plot.m
@@ -0,0 +1,13 @@
+close all;
+
+FS = 20;
+LW = 2;
+
+figure;
+
+plot(out.y)
+grid on
+legend('y', 'Location','best', 'FontSize', FS);
+
+set(findall(gcf,'-property','FontSize'),'FontSize',FS)
+set(findall(gcf,'Type','Line'),'LineWidth',LW)
